a023e0dd5ac0bc96ad54574b48142d11d207d633
turbokongen <turbokongen@hotmail.com>
2019-11-08 16:16:23 +0100

    Wrong arguments

--- ams/sensor.py
+++ ams/sensor.py
@@ -30,7 +30,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
-def setup_platform(hass, add_devices):
+def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Serial sensor platform."""
     sensor_states = {}
     sensors = []
