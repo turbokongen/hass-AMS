cc092709fa7b757982eda3441541c1f35db75e60
turbokongen <turbokongen@hotmail.com>
2020-03-14 16:26:43 +0000

    read inWaiting() instead of stream.

--- custom_components/ams/__init__.py
+++ custom_components/ams/__init__.py
@@ -5,6 +5,7 @@ import serial
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import Config, HomeAssistant
 from homeassistant.helpers.dispatcher import async_dispatcher_send
+from memory_profiler import profile
 from .parsers import kaifa as Kaifa
 from .parsers import kamstrup as Kamstrup
 from .parsers import aidon as Aidon
@@ -56,6 +57,7 @@ async def async_remove_entry(hass, entry) -> None:
 class AmsHub:
     """AmsHub wrapper for all sensors."""
 
+    @profile(precision=6)
     def __init__(self, hass, entry):
         """Initialize the AMS hub."""
         self._hass = hass
@@ -76,6 +78,7 @@ class AmsHub:
         self.connection.start()
         _LOGGER.debug('Finish init of AMS')
 
+    @profile(precision=6)
     def stop_serial_read(self):
         """Close resources."""
         _LOGGER.debug("stop_serial_read")
@@ -84,6 +87,7 @@ class AmsHub:
         self._ser.close()
         self.connection.join()
 
+    @profile(precision=6)
     def read_bytes(self):
         """Read the raw data from serial port."""
         byte_counter = 0
@@ -98,6 +102,7 @@ class AmsHub:
             else:
                 continue
 
+    @profile(precision=6)
     def connect(self):
         """Read the data from the port."""
         if self.meter_manufacturer == "kaifa":
@@ -109,16 +114,15 @@ class AmsHub:
         while self._running:
             try:
                 data = self.read_bytes()
+                _LOGGER.debug('reading data = %s', data)
                 if parser.test_valid_data(data):
                     _LOGGER.debug("data read from port=%s", data)
                     self.sensor_data = parser.parse_data(
                         self.sensor_data, data)
                     self._hass.data[AMS_SENSORS] = self.sensor_data
                     self._check_for_new_sensors_and_update(self.sensor_data)
-                    _LOGGER.debug('buffer: %s', self._ser.inWaiting())
                 else:
                     self._ser.flushInput()
-                    _LOGGER.debug('buffer: %s', self._ser.inWaiting())
                     _LOGGER.debug("failed package: %s", data)
             except serial.serialutil.SerialException:
                 pass
@@ -129,6 +133,7 @@ class AmsHub:
         _LOGGER.debug('sending sensor data')
         return self.sensor_data
 
+    @profile(precision=6)
     def _check_for_new_sensors_and_update(self, sensor_data):
         """Compare sensor list and update."""
         sensor_list = []
