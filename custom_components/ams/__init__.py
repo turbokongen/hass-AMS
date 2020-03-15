fdb8278e3210971970306f58b99f0a33e8bcf1e9
turbokongen <turbokongen@hotmail.com>
2020-03-15 07:59:07 +0000

    mistakenly removed a line

--- custom_components/ams/__init__.py
+++ custom_components/ams/__init__.py
@@ -115,6 +115,7 @@ class AmsHub:
             parser = Kamstrup
         while self._running:
             try:
+                data = self.read_bytes()
                 _LOGGER.debug('reading data = %s', data)
                 if parser.test_valid_data(data):
                     _LOGGER.debug(data)
