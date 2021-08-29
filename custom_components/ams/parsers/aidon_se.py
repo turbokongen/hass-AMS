71ab35541fc9cd4325211af357e72ad80b5ee0d4
turbokongen <turbokongen@hotmail.com>
2021-08-29 09:35:14 +0200

    Wrongly placed hourly sensor data in aidon_Se

--- custom_components/ams/parsers/aidon_se.py
+++ custom_components/ams/parsers/aidon_se.py
@@ -100,15 +100,6 @@ def parse_data(stored, data):
                             measure = byte_decode(fields=pkt[v_start:v_stop])
                             if key in const.HOURLY_SENSORS:
                                 han_data[key] = measure / 1000
-                                sensor_data[key][const.SENSOR_ATTR][
-                                    const.ATTR_STATE_CLASS] = (
-                                        const.STATE_CLASS_MEASUREMENT)
-                                sensor_data[key][const.SENSOR_ATTR][
-                                    const.ATTR_DEVICE_CLASS] = (
-                                        const.DEVICE_CLASS_ENERGY)
-                                sensor_data[key][const.SENSOR_ATTR][
-                                    const.ATTR_LAST_RESET] = (
-                                        const.LAST_RESET_DATA)
                             else:
                                 han_data[key] = measure
                             sensor_data[key] = {
@@ -129,6 +120,16 @@ def parse_data(stored, data):
                                         const.SENSOR_ICON_MAP.get(key)),
                                 },
                             }
+                            if key in const.HOURLY_SENSORS:
+                                sensor_data[key][const.SENSOR_ATTR][
+                                    const.HAN_METER_DATETIME] = han_data[
+                                    const.HAN_METER_DATETIME]
+                                sensor_data[key][const.SENSOR_ATTR][
+                                    const.ATTR_STATE_CLASS] = (
+                                    const.STATE_CLASS_TOTAL_INCREASING)
+                                sensor_data[key][const.SENSOR_ATTR][
+                                    const.ATTR_DEVICE_CLASS] = (
+                                    const.DEVICE_CLASS_ENERGY)
                             _LOGGER.debug(
                                 "%s, OBIS:%s, Index:%s, Type:%s Double OBIS",
                                 key, item, (i, i + len(item)),
