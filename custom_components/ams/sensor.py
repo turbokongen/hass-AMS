cd8249b8843856f7213d03fa0338d383178059cb
Oyvind Eriksen <oyvind@eriksen.cn>
2021-12-13 21:23:40 +0100

    Fix device_state_attributes warnings

--- custom_components/ams/sensor.py
+++ custom_components/ams/sensor.py
@@ -135,7 +135,7 @@ class AmsSensor(RestoreEntity):
         return False
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the attributes of the entity (if any JSON present)."""
         return self._attributes
 
